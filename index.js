7c695fa7c1adbf6588e49d97e4e2b8e52af58f6d
Anandkumar Patel <avpatel2005@gmail.com>
2023-04-05 23:09:24 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", changeText);
+
+function changeText(e){
+    let p = document.querySelector("p");
+    p.textContent = "This is really cool!";
+}
+
+
